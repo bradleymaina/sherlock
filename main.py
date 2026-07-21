15d92465ba267841db5a6837185e8db4904d8891
bradleymaina <bradleymaina@proton.me>
2026-07-21 19:57:44 +0300

    fixed database logic and added gitignore file

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import sqlite3
-
-conn = sqlite3.connect("lecturers.db")
+ 
+# use context manager to connect to the database
+conn = sqlite3.connect("lecturer.db")
 csr = conn.cursor()
 
-csr.execute('''CREATE TABLE IF NOT EXISTS lecturers (
+csr.execute('''CREATE TABLE IF NOT EXISTS lecturer_table (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    contact TEXT NOT NULL
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    phone_number TEXT NOT NULL
     ) ''')
 
 
-public class Lecturer {
-    private int id = 0;
-    private String name = input("Enter lecturer name: ");
-    private String contact = input("Enter lecturer contact: ");
+country_code = "+254";
+id = 0;
+first_name = input("Enter lecturer first name: ").strip();
+last_name = input("Enter lecturer last name: ").strip();
+phone_number = input(f"Enter lecturer phone number: {country_code}").strip();
+
+phone_number = country_code + phone_number;
+
+if len(phone_number) != 13:
+    print("Invalid phone number. Please enter a valid 13 digit phone number.")
+    exit()
+
+
+csr.execute('''INSERT INTO lecturer_table (first_name, last_name, phone_number) VALUES (?, ?, ?)''', (first_name, last_name, phone_number))
+conn.commit()
+
+print("Lecturer added successfully!")
+
+conn.close()
