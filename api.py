4c07bc73df613001b56e480b9eeb2bc3570ec2b4
bradleymaina <bradleymaina@proton.me>
2026-07-26 00:55:03 +0300

    feat: Expose functions in main.py as api enpoints

--- api.py
+++ api.py
@@ -1,20 +1,28 @@
 from fastapi import FastAPI  
+from pydantic import BaseModel
+
 from main import add_lecturer , lecturer_lookup
 
-app = FastAPI()
 
-@app.post("/lecturers")
-def create_lecturer(first_name: str, last_name: str, phone_number: str):
-    result = add_lecturer(
-        first_name,
-        last_name,
-        phone_number
-    )
-    return result
+class Lecturer(BaseModel):
+    first_name: str
+    last_name: str
+    phone_number: str
 
-@app.get("/lecturers/search")
-def get_lecturer(search_name: str):
-    result = lecturer_lookup(
-        search_name
-    )
-    return result
+    app = FastAPI()
+
+    @app.post("/lecturers")
+    def create_lecturer(first_name: str, last_name: str, phone_number: str):
+        result = add_lecturer(
+            first_name,
+            last_name,
+            phone_number
+        )
+        return result
+
+    @app.get("/lecturers/search")
+    def get_lecturer(search_name: str):
+        result = lecturer_lookup(
+            search_name
+        )
+        return result
