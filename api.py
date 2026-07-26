fd21c35187cdab2e77eec3b1626ecf4ebf947593
bradleymaina <bradleymaina@proton.me>
2026-07-26 03:21:41 +0300

    feat: Add Pydantic for data validation
    BREAKING CHANGE
    This changes requests from url to objects
    This validates data at runtime

--- api.py
+++ api.py
@@ -1,28 +1,37 @@
 from fastapi import FastAPI  
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from main import add_lecturer , lecturer_lookup
 
+app = FastAPI()
+
 
 class Lecturer(BaseModel):
-    first_name: str
-    last_name: str
-    phone_number: str
+    first_name: str = Field(
+        min_length  = 3,
+        max_length = 10,
+        pattern=r"^[A-Za-z]+$")
+    last_name: str = Field(
+        min_length = 3 ,
+        max_length = 10,
+        pattern=r"^[A-Za-z]+$")
+    phone_number: str = Field(
+         min_length = 10,
+        max_length = 10,
+        pattern= r"^(07|01)\d{8}$")
 
-    app = FastAPI()
+@app.post("/lecturers")
+def create_lecturer(lecturer: Lecturer):
+    result = add_lecturer(
+        lecturer.first_name,
+        lecturer.last_name,
+        lecturer.phone_number
+    )
+    return result
 
-    @app.post("/lecturers")
-    def create_lecturer(first_name: str, last_name: str, phone_number: str):
-        result = add_lecturer(
-            first_name,
-            last_name,
-            phone_number
-        )
-        return result
-
-    @app.get("/lecturers/search")
-    def get_lecturer(search_name: str):
-        result = lecturer_lookup(
-            search_name
-        )
-        return result
+@app.get("/lecturers/search")
+def get_lecturer(search_name:str ):
+    result = lecturer_lookup(
+        search_name
+    )
+    return result
