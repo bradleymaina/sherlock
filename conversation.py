65fdb2ea16692b879d6dd3827f6c7c57a6b1476b
bradleymaina <bradleymaina@proton.me>
2026-07-30 03:54:08 +0300

    feat: Add conversation states

---
+++ conversation.py
@@ -0,0 +1,41 @@
+START = "START"
+MENU = "MENU"
+
+#Add Lecturer
+WAITING_FOR_FIRST_NAME = "WAITING_FOR_FIRST_NAME"
+WAITING_FOR_LAST_NAME = "WAITING_FOR_LAST_NAME"
+WAITING_FOR_PHONE_NUMBER = "WAITING_FOR_PHONE_NUMBER"
+
+#Search for Lecturer
+WAITING_FOR_SEARCH_NAME = "WAITING_FOR_SEARCH_NAME"
+SHOWING_SEARCH_RESULTS = "SHOWING_SEARCH_RESULTS"
+
+states = {}
+
+def set_state(phone, state):
+    states[phone] = state
+
+def get_state(phone):
+    return states.get(phone)
+
+def process_message(phone, msg):
+    state = get_state(phone)
+
+    if state is None:
+        set_state(phone, START)
+        state = START
+    elif state == MENU:
+
+        if msg == "add_lecturer":
+            set_state(phone, WAITING_FOR_FIRST_NAME) 
+
+        elif msg == "search_lecturer":
+            set_state(phone, WAITING_FOR_SEARCH_NAME)
+
+    elif state == WAITING_FOR_FIRST_NAME:
+        set_state(phone, WAITING_FOR_LAST_NAME)
+
+    elif state == WAITING_FOR_LAST_NAME:
+        set_state(phone, WAITING_FOR_PHONE_NUMBER)
+
+    elif state == WAITING_FOR_PHONE_NUMBER
